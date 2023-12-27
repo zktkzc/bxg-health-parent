1d4b3fdfb6d79902022b82084a5a2ead543c1fd2
tkzc00 <2238983030@qq.com>
2023-12-27 14:45:49 +0800

    完成用户端登录校验功能

--- bxg-health-mobile/src/main/java/com/itheima/controller/MemberController.java
+++ bxg-health-mobile/src/main/java/com/itheima/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.itheima.controller;
 import com.itheima.common.constant.MessageConstant;
 import com.itheima.common.dto.LoginDTO;
 import com.itheima.common.entity.Result;
+import com.itheima.mapper.MemberMapper;
+import com.itheima.pojo.Member;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @Resource
     private RedisTemplate redisTemplate;
+    @Resource
+    private MemberMapper memberMapper;
 
     /**
      * 手机验证码登录
@@ -29,6 +34,13 @@ public class MemberController {
             if (!code.equals(loginDTO.getValidateCode())) {
                 return Result.fail(MessageConstant.VALIDATECODE_ERROR);
             }
+        }
+        Member member = memberMapper.selectByPhone(loginDTO.getTelephone());
+        if (member == null) {
+            member = new Member();
+            member.setPhoneNumber(loginDTO.getTelephone());
+            member.setRegTime(new Date());
+            memberMapper.insert(member);
         }
         return Result.success("登录成功");
     }
--- bxg-health-mobile/src/main/java/com/itheima/mapper/MemberMapper.java
+++ bxg-health-mobile/src/main/java/com/itheima/mapper/MemberMapper.java
@@ -1,6 +1,7 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Member;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,4 +14,11 @@ public interface MemberMapper {
 
     @Select("select * from t_member where id=#{memberId}")
     Member selectById(Integer memberId);
+
+    @Select("select * from t_member where phoneNumber=#{telephone}")
+    Member selectByPhone(String telephone);
+
+    @Insert("insert into t_member(fileNumber, name, sex, idCard, phoneNumber, regTime, password, email, birthday, remark) " +
+            "values (#{fileNumber} ,#{name} ,#{sex} ,#{idCard} ,#{phoneNumber} ,#{regTime} ,#{password} ,#{email} ,#{birthday} ,#{remark})")
+    void insert(Member member);
 }
