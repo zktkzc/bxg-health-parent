59e4f75d2f21cb29d9576c3f185c3fcfd9d470b1
tkzc00 <2238983030@qq.com>
2023-12-27 17:33:43 +0800

    完善用户端体检预约的功能

--- bxg-health-mobile/src/main/java/com/itheima/mapper/MemberMapper.java
+++ bxg-health-mobile/src/main/java/com/itheima/mapper/MemberMapper.java
@@ -7,9 +7,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MemberMapper {
-    @Select("select * from t_member where name=#{name}")
-    Member selectByName(String name);
-
     void update(Member member);
 
     @Select("select * from t_member where id=#{memberId}")
