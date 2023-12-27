3f72c85af5184962781b6becb27c2a8b7ed7014c
tkzc00 <2238983030@qq.com>
2023-12-27 16:43:32 +0800

    完善用户端redis存储验证码的功能

--- bxg-health-mobile/src/main/java/com/itheima/controller/ValidateCodeController.java
+++ bxg-health-mobile/src/main/java/com/itheima/controller/ValidateCodeController.java
@@ -1,14 +1,12 @@
 package com.itheima.controller;
 
 import com.itheima.common.constant.MessageConstant;
+import com.itheima.common.dto.ValidateDTO;
 import com.itheima.common.entity.Result;
 import com.itheima.common.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -24,11 +22,12 @@ public class ValidateCodeController {
      * 发送短信验证码
      */
     @GetMapping("/send4Login")
-    public Result send4Login(String telephone) {
+    public Result send4Login(@RequestBody ValidateDTO validateDTO) {
         // 生成验证码
         String validateCode = ValidateCodeUtils.generateValidateCode4String(6);
-        log.info("发送登录验证码成功，手机号：{}，验证码：{}", telephone, validateCode);
-        redisTemplate.opsForValue().set("ValidateCode4Login_" + telephone, validateCode, 5, TimeUnit.MINUTES);
+        log.info("发送登录验证码成功，手机号：{}，验证码：{}", validateDTO.getTelephone(), validateCode);
+        redisTemplate.delete("ValidateCode4Login_" + validateDTO.getTelephone());
+        redisTemplate.opsForValue().set("ValidateCode4Login_" + validateDTO.getTelephone(), validateCode, 5, TimeUnit.MINUTES);
         return Result.success(MessageConstant.SEND_VALIDATECODE_SUCCESS, validateCode);
     }
 
@@ -36,11 +35,12 @@ public class ValidateCodeController {
      * 体检预约发送验证码
      */
     @PostMapping("/send4Order")
-    public Result send4Order(String telephone) {
+    public Result send4Order(@RequestBody ValidateDTO validateDTO) {
         // 生成验证码
         String validateCode = ValidateCodeUtils.generateValidateCode4String(4);
-        log.info("发送预约验证码成功，手机号：{}，验证码：{}", telephone, validateCode);
-        redisTemplate.opsForValue().set("ValidateCode4Order_" + telephone, validateCode, 5, TimeUnit.MINUTES);
+        log.info("发送预约验证码成功，手机号：{}，验证码：{}", validateDTO.getTelephone(), validateCode);
+        redisTemplate.delete("ValidateCode4Order_" + validateDTO.getTelephone());
+        redisTemplate.opsForValue().set("ValidateCode4Order_" + validateDTO.getTelephone(), validateCode, 5, TimeUnit.MINUTES);
         return Result.success(MessageConstant.SEND_VALIDATECODE_SUCCESS, validateCode);
     }
 }
